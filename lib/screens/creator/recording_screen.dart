91194b087450c5c9cd8f0066b42a13836a4f3cda
Isaac <tolulopesoliu2005@gmail.com>
2025-10-02 16:31:49 +0100

    Update recording screen

--- lib/screens/creator/recording_screen.dart
+++ lib/screens/creator/recording_screen.dart
@@ -5,7 +5,7 @@ import 'dart:io'; // For file I/O
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:edunote/models/class_model.dart';
 import 'package:edunote/utils/colour.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
